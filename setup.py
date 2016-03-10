894a129f96dce9e2bae2dd5c2d524e35114adcf6
Zach Garwood <zachgarwood@gmail.com>
2016-03-09 22:18:28 -0600

    Added project config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="credit_card",
+    description="A code challenge from Cloudspotter",
+    author="Zach Garwood",
+    author_email="zachgarwood@gmail.com",
+    license="MIT",
+    packages=["credit-card"],
+)
