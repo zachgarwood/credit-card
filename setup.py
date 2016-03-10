d7d828e81055e20fababcde1202a57c5544ad4a9
Zach Garwood <zachgarwood@gmail.com>
2016-03-09 22:29:53 -0600

    Tweaked various configs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="credit_card",
+    name="credit-card",
     description="A code challenge from Cloudspotter",
     author="Zach Garwood",
     author_email="zachgarwood@gmail.com",
