bf0d8c7bc0eb574f68c5734e0ebf5cdd34aadb65
Zach Garwood <zachgarwood@gmail.com>
2016-03-10 00:43:53 -0600

    Added `Ledger.charge()` method

--- credit_card/ledger.py
+++ credit_card/ledger.py
@@ -2,13 +2,16 @@ class Ledger:
     def __init__(self):
         self.accounts = {}
 
-    def add(self, subject, number, limit):
-        self.accounts[subject] = {'number': number,
-                                  'limit': limit,
-                                  'balance': 0}
+    def add(self, name, number, limit):
+        self.accounts[name] = {'number': number,
+                               'limit': int(limit),
+                               'balance': 0}
 
-    def charge(self, subject, *args):
-        print('charge')
+    def charge(self, name, amount):
+        amount = int(amount)
+        account = self.accounts[name]
+        if ((account['balance'] + amount) <= account['limit']):
+            account['balance'] += amount
 
-    def credit(self, subject, *args):
+    def credit(self, name, *args):
         print('credit')
--- test/ledger_test.py
+++ test/ledger_test.py
@@ -3,17 +3,34 @@ import unittest
 
 
 class LedgerTest(unittest.TestCase):
+    def setUp(self):
+        self.ledger = Ledger()
+
     def test_commands_exist(self):
         for command_name in ['add', 'charge', 'credit']:
             self.assertTrue(hasattr(Ledger, command_name),
                             'Ledger class does not contain ' + command_name)
 
     def test_add(self):
-        ledger = Ledger()
-        ledger.add('Test', '12345', 1000)
+        self.ledger.add('Test', '12345', 1000)
 
-        self.assertIn('Test', ledger.accounts)
+        self.assertIn('Test', self.ledger.accounts)
         self.assertDictEqual(
             {'balance': 0, 'limit': 1000, 'number': '12345'},
-            ledger.accounts['Test']
+            self.ledger.accounts['Test'],
+            'The limit and number were not set and/or the balance was not '
+            'initialized to 0'
         )
+
+    def test_charge(self):
+        self.ledger.add('Test', '12345', 1000)
+        self.ledger.charge('Test', 500)
+
+        self.assertEqual(500,
+                         self.ledger.accounts['Test']['balance'],
+                         'The account balance was not increased')
+
+        self.ledger.charge('Test', 501)
+        self.assertNotEqual(1001,
+                            self.ledger.accounts['Test']['balance'],
+                            'The account balance increased beyone the limit')
